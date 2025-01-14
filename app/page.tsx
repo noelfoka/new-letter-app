51fb4305bf20e62f4b05b3a13648f2189658bfea
noelfoka <nomgnenoel@gmail.com>
2025-01-14 15:33:26 +0100

    ajout d'un input

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       {/* Toas */}
       <div className="flex items-center mb-6 md:w-[600px]">
         <div className="flex flex-col items-center w-full">
-          <div className="h-[200px] bg-[url('/img.jpg')] w-full bg-cover bg-center rounded-3xl mb-6"></div>
+          <div className="h-[200px] bg-[url('/banner.png')] w-full bg-cover bg-center rounded-3xl mb-6"></div>
           <div className="w-fullp-10 pt-0">
             <h1 className="text-2xl md:text-4xl font-bold text-white">
               Bienvenue à ma new letter
@@ -15,8 +15,14 @@ export default function Home() {
               Recevez des nouvelles et des conseils pour améliorer votre projet
             </p>
             <form className="flex items-center w-full">
-              <input type="text" className="input input-bordered w-full" placeholder="email@example.com" />
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="email@example.com"
+              />
+              <button className="btn btn-primary ml-3">S&apos;abonner</button>
             </form>
+            <p>Pas de spam, pas de publicité, pas de newsletters inutiles</p>
           </div>
         </div>
       </div>
