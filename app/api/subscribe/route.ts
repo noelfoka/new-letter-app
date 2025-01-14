0da4167080d1500c4506c08857c5ecc568771c59
noelfoka <nomgnenoel@gmail.com>
2025-01-14 19:16:21 +0100

    setup mailchimp to build api

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,1 +1,7 @@
 import mailchimp from "@mailchimp/mailchimp_marketing";
+
+// configuration de mailchimp
+const mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY!,
+  server: process.env.MAILCHIMP_API_SERVER!
+});
