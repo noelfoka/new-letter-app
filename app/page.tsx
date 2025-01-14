aaa1c64546f58c2e7cc2e28a5b140d3dcdb640f8
noelfoka <nomgnenoel@gmail.com>
2025-01-14 15:26:31 +0100

    mise à jour

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,18 @@ export default function Home() {
       {/* Toas */}
       <div className="flex items-center mb-6 md:w-[600px]">
         <div className="flex flex-col items-center w-full">
-
-        <div className="h-[200px] bg-[url('/img.jpg')] w-full bg-cover bg-center rounded-3xl mb-6">
-          
-        </div>
-        <div className="w-fullp-10 pt-0">
-          <h1 className="text-2xl md:text-4xl font-bold text-white">Bienvenue à ma new letter</h1>
-          <p className="my-4 text-sm">Recevez des nouvelles et des conseils pour améliorer votre projet</p>
-        </div>
+          <div className="h-[200px] bg-[url('/img.jpg')] w-full bg-cover bg-center rounded-3xl mb-6"></div>
+          <div className="w-fullp-10 pt-0">
+            <h1 className="text-2xl md:text-4xl font-bold text-white">
+              Bienvenue à ma new letter
+            </h1>
+            <p className="my-4 text-sm">
+              Recevez des nouvelles et des conseils pour améliorer votre projet
+            </p>
+            <form className="flex items-center w-full">
+              <input type="text" className="input input-bordered w-full" placeholder="email@example.com" />
+            </form>
+          </div>
         </div>
       </div>
     </div>
