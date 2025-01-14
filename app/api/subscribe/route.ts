cb9e14d64171412be98e4275957fc3dfef3377c3
noelfoka <nomgnenoel@gmail.com>
2025-01-14 19:30:47 +0100

    récupération de l'email

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,7 +1,23 @@
 import mailchimp from "@mailchimp/mailchimp_marketing";
+import { NextResponse } from "next/server";
 
 // configuration de mailchimp
-const mailchimp.setConfig({
+mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY!,
-  server: process.env.MAILCHIMP_API_SERVER!
+  server: process.env.MAILCHIMP_API_SERVER!,
 });
+
+// route pour l'api
+
+export async function POST(request: Request) {
+  try {
+    // récupération de l'email
+    const email = await request.json();
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Cette adresse email est déjà inscrite ou n'existe pas" },
+      { status: 500 }
+    );
+    console.log(error);
+  }
+}
